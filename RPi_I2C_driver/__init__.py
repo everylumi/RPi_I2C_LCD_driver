c796b3847c79fd50edaa85c4d38e26a12d9e7782
Github <Github>
2021-10-31 23:02:45 +0900

    [Issued at Oct-31, 2021]

---
+++ RPi_I2C_driver/__init__.py
@@ -0,0 +1,1 @@
+from . import RPi_I2C_driver
---
+++ RPi_I2C_driver/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='RPi_I2C_driver',
+    packages=['.'],
+    version='1.0.0',
+    description='I2C_LCD_driver interface library',
+    author='everylumi@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+)
